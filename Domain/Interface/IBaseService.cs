fc1d1929ef0e0fcf1ad39dbe5c28bf3e4603001f
thalis.mraz <thalis.mraz@gmail.com>
2021-09-29 20:44:18 -0300

    Add project files.

---
+++ Domain/Interface/IBaseService.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface
+{
+    //TEntity é genérico e aceita qualquer classe passada como parâmetro.
+    public interface IBaseService<TEntity> where TEntity: Entity
+    {
+        //AbstractValidator ferramenta que vai auxiliar validação
+        TEntity Add<TValidator>(TEntity obj) where TValidator : AbstractValidator<TEntity>;
+
+        void Delete(int id);
+
+        IList<TEntity> Get();
+
+        TEntity GetById(int id);
+
+        TEntity Update<TValidator>(TEntity obj) where TValidator : AbstractValidator<TEntity>;
+    }
+}
